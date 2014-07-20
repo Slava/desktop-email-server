627c2bfa9e28e5254cbac0a6e0108da7b691220c
Matthew Tancik <tancik@mit.edu>
2014-07-20 00:31:48 -0700

    Added pinging from client to server + bootstrap-flatui to smart.json

--- client/index.js
+++ client/index.js
@@ -1,16 +1,18 @@
-if (Meteor.isServer) {
-} else {
-  Template.main.events({
-    'click .login': function () {
-      Meteor.loginWithGoogle({
-        requestPermissions: ['email',
-          "https://mail.google.com/",
-          "https://www.googleapis.com/auth/gmail.readonly",
-          "https://www.googleapis.com/auth/gmail.modify"
-        ],
-        requestOfflineToken: true
-      });
-    }
-  });
-}
+Template.main.events({
+  'click .login': function () {
+    Meteor.loginWithGoogle({
+      requestPermissions: ['email',
+        "https://mail.google.com/",
+        "https://www.googleapis.com/auth/gmail.readonly",
+        "https://www.googleapis.com/auth/gmail.modify"
+      ],
+      requestOfflineToken: true
+    });
+  }
+});
+
+// every 4 minutes, notify server that we still care about email updates
+Meteor.setTimeout(function () {
+  Meteor.call("ping");
+}, 4 * 60 * 1000);
 
